b0f989183fc01164bce5c7f83ac2434d1a2cf366
Coaxial <thecoaxial@gmail.com>
2021-03-15 15:53:45 -0400

    Added routes to new pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
 import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 import './App.css';
+import Portfolio from "./pages/Portfolio/Portfolio";
+
 
 
 function App() {
@@ -12,8 +16,10 @@ function App() {
       <Navbar />
         
         <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
          
-      
+      <Footer />
       
     </div>
   </Router>
